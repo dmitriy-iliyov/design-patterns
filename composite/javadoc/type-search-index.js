05a870dd7ba6e0a22b01560de7f806c26682989d
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-01-27 20:35:37 +0200

    composite

---
+++ composite/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Component"},{"p":"org.example","l":"Composite"},{"p":"org.example","l":"Element"},{"p":"org.example","l":"Main"}]
---
+++ composite/src/main/java/org/example/Composite.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Composite implements Component{
+    private ArrayList<Component> components;
+    private int area;
+
+    public Composite() {
+        this.components = new ArrayList<>();
+    }
+
+    public void addComponent(Component component) {
+        components.add(component);
+    }
+
+    public Component getComponent(int index) {
+        System.out.println(components.get(index));
+        return components.get(index);
+    }
+
+    @Override
+    public int calcArea() {
+        if (this.area != 0){
+            this.area = 0;
+        }
+        for(Component component : components){
+            this.area += component.calcArea();
+        }
+        System.out.println(this.area);
+        return this.area;
+    }
+}
