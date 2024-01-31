7b630a4d9235db1b7bc7676c6b9bac3dbd7d1f5d
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-01-31 11:30:43 +0200

    modified Main.java

--- strategy/src/main/java/org/example/Main.java
+++ strategy/src/main/java/org/example/Main.java
@@ -3,14 +3,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        bubble insert shell selection
-
+        Sort sort = new BubbleSort();
         Collection collection = new Collection(20);
-        collection.sortArray("insert", 1);
+        collection.setSort(sort);
         collection.printArray();
-        collection.sortArray("insert", 0);
+        collection.sortArray(true);
         collection.printArray();
-        collection.sortArray("bubble", 1);
+        collection.sortArray(false);
+        collection.printArray();
+        Sort sort1 = new SelectionSort();
+        collection.setSort(sort1);
+        collection.printArray();
+        collection.sortArray(true);
+        collection.printArray();
+        collection.sortArray(false);
+        collection.printArray();
+        Sort sort2 = new ShellSort();
+        collection.setSort(sort2);
+        collection.printArray();
+        collection.sortArray(true);
+        collection.printArray();
+        collection.sortArray(false);
+        collection.printArray();
+        Sort sort3 = new InserSort();
+        collection.setSort(sort3);
+        collection.printArray();
+        collection.sortArray(true);
+        collection.printArray();
+        collection.sortArray(false);
         collection.printArray();
     }
 }
